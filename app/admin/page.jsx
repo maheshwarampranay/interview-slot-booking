b5bf9dd7b4ae43ad5d27a08efcab915bcb6a0d0d
mdimado <mdimad005@gmail.com>
2025-01-08 23:37:27 +0530

    resolve errors

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Alert, AlertDescription } from '@/components/ui/alert';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { db } from '@/utils/firebase';
 import * as XLSX from 'xlsx';
 import { Loader2 } from 'lucide-react';
